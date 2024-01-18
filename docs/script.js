5f65b39849ee30771d6baa2efed9fac6eb653240
Kesselor <122703033+Kesselor@users.noreply.github.com>
2024-01-18 18:40:53 +0100

    newstart

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('load', function() {
-    var loader = document.querySelector('.load-animation');
-    var mainContent = document.querySelector('main');
-    setTimeout(function() {
-        loader.style.display = 'none';
-        mainContent.style.opacity = 1; // Ensures the main content is visible after loading
-    }, 2000); // Adjust time to match the duration of the load animation
-});
