b9a7163b221eb8533adb227e63de4f91a5f0f10b
Kesselor <122703033+Kesselor@users.noreply.github.com>
2024-01-18 17:46:57 +0100

    Animation

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,7 @@
 
-// JavaScript code for dynamic content can be added here
+window.addEventListener('load', function() {
+    var loader = document.querySelector('.load-animation');
+    setTimeout(function() {
+        loader.style.display = 'none';
+    }, 2000); // Hides the animation after 2 seconds
+});
