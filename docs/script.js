318d991a46689aa46732bf3082fbfe73c7a9bf9b
Kesselor <122703033+Kesselor@users.noreply.github.com>
2024-01-18 17:46:57 +0100

    Animation

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,7 @@
 
 window.addEventListener('load', function() {
     var loader = document.querySelector('.load-animation');
-    var mainContent = document.querySelector('main');
     setTimeout(function() {
         loader.style.display = 'none';
-        mainContent.style.opacity = 1; // Ensures the main content is visible after loading
-    }, 2000); // Adjust time to match the duration of the load animation
+    }, 2000); // Hides the animation after 2 seconds
 });
