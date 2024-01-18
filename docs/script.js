ce9b3fde3f260e1f9bbeb1d8e0cf2bdf39d6d8de
Kesselor <122703033+Kesselor@users.noreply.github.com>
2024-01-18 17:52:15 +0100

    Animation 2

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,9 @@
 
 window.addEventListener('load', function() {
     var loader = document.querySelector('.load-animation');
+    var mainContent = document.querySelector('main');
     setTimeout(function() {
         loader.style.display = 'none';
-    }, 2000); // Hides the animation after 2 seconds
+        mainContent.style.opacity = 1; // Ensures the main content is visible after loading
+    }, 2000); // Adjust time to match the duration of the load animation
 });
